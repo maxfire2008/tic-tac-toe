b0bcfaa633e8f907205d9ab0cd1aa40072f9ca9e
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2023-03-28 09:19:24 +1100

    finish basic python game without win checking

--- ttt.py
+++ ttt.py
@@ -2,18 +2,18 @@
 
 class Board:
     def __init__(self):
-        self.a = "   "
-        self.b = "   "
-        self.c = "   "
+        self.a = list("   ")
+        self.b = list("   ")
+        self.c = list("   ")
 
 def board_render(board):
     board_ascii = ""
     board_ascii += "A "
-    board_ascii += board.a
+    board_ascii += ''.join(board.a)
     board_ascii += "\nB "
-    board_ascii += board.b
+    board_ascii += ''.join(board.b)
     board_ascii += "\nC "
-    board_ascii += board.c
+    board_ascii += ''.join(board.c)
     return board_ascii
 
 def main():
@@ -22,14 +22,23 @@ def main():
     while 1:
         board_ascii = board_render(board)
         print(board_ascii)
+        print(current_move+" move:", end="")
+        move = input("")[:2]
+        print(current_move+" chose", move)
+        move_Y = move[0]
+        move_X = ord(move[1])-49
+        print("Y:", ord(move_Y))
+        print("X:", move_X)
+        if move_Y == "A":
+            board.a[move_X] = current_move
+        if move_Y == "B":
+            board.b[move_X] = current_move
+        if move_Y == "C":
+            board.c[move_X] = current_move
         if current_move == "X":
-            print("X move:", end="")
-            move = input("")[:2]
-            print("X chose", move)
-            move_Y = move[0]
-            move_X = move[1]
-            print("Y:", ord(move_Y))
-            print("X:", ord(move_X)-49)
+            current_move = "Y"
+        else:
+            current_move = "X"
     return 0
 
 if __name__ == "__main__":
