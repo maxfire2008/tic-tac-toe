d405e042459eb24640b80996a16b8be2b50aeedb
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2023-03-30 11:28:37 +1100

    tried to implement win checking

--- ttt.c
+++ ttt.c
@@ -1,6 +1,32 @@
 #include<stdio.h>
 #include<string.h>
 
+// def player_won(player, board):
+int player_won(char player, char board[3][4]) {
+//     for y in range(3):
+    char y;
+    for (y = 1; y < 3; ++y) {
+//         if board[y][0] == player and board[y][1] == player and board[y][2] == player:
+        printf("\n==\n%s\n==\n", board[y]);
+//         if (
+//             strcmp(&board[y][0], &player) &&
+//             strcmp(&board[y][1], &player) &&
+//             strcmp(&board[y][2], &player)
+//         ) {
+// //             return 1
+//             return 1;
+//         }
+    }
+//     for x in range(3):
+//         if board[0][x] == player and board[1][x] == player and board[2][x] == player:
+//             return 1
+//     if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+//         return 1
+//     if board[0][2] == player and board[1][1] == player and board[2][0] == player:
+//         return 1
+//     return 0
+    return 0;
+}
 
 // def board_render(board):
 void board_render(char board[3][4], char *board_ascii) {
@@ -49,6 +75,16 @@ int main() {
         board_render(board, board_ascii);
 //         print(board_ascii)
         printf("%s\n\n", board_ascii);
+        // if player_won("X", board):
+        if (player_won("X", board)) {
+        //     print("Player X has won!")
+            printf("Player X has won!");
+            break;
+        //     break
+        }
+        // if player_won("Y", board):
+        //     print("Player Y has won!")
+        //     break
 //         print(current_move+" move:", end="")
         printf("%s move:", current_move);
 //         move = input("")[:2]
--- ttt.py
+++ ttt.py
@@ -1,12 +1,25 @@
 #!python3
 
+def player_won(player, board):
+    for y in range(3):
+        if board[y][0] == player and board[y][1] == player and board[y][2] == player:
+            return 1
+    for x in range(3):
+        if board[0][x] == player and board[1][x] == player and board[2][x] == player:
+            return 1
+    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        return 1
+    if board[0][2] == player and board[1][1] == player and board[2][0] == player:
+        return 1
+    return 0
+
 def board_render(board):
-    board_ascii = ""
-    board_ascii += "a "
+    board_ascii = "  123\n"
+    board_ascii += "a|"
     board_ascii += ''.join(board[0])
-    board_ascii += "\nb "
+    board_ascii += "\nb|"
     board_ascii += ''.join(board[1])
-    board_ascii += "\nc "
+    board_ascii += "\nc|"
     board_ascii += ''.join(board[2])
     return board_ascii
 
@@ -20,6 +33,13 @@ def main():
     while 1:
         board_ascii = board_render(board)
         print(board_ascii)
+        if player_won("X", board):
+            print("Player X has won!")
+            break
+        if player_won("Y", board):
+            print("Player Y has won!")
+            break
+
         print(current_move+" move:", end="")
         move = input("")[:2]
         print(current_move+" chose", move)
